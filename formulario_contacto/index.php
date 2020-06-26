0b6b6b0f6388a7c60e8d91fa477b51a5f8b970a1
David s <dadiortiz403@gmail.com>
2020-06-25 21:03:39 -0500

    Finalizacion de la practica

---
+++ formulario_contacto/index.php
@@ -0,0 +1,53 @@
+<?php
+
+$errores = '';
+$enviado = '';
+
+
+if (isset($_POST['submit'])){
+    $nombre = $_POST['nombre'];
+    $correo = $_POST['correo'];
+    $mensaje = $_POST['mensaje'];
+
+    if(!empty($nombre)){
+        $nombre = trim($nombre);
+        $nombre = filter_var($nombre, FILTER_SANITIZE_STRING);
+    } else {
+        $errores .= 'Por favor ingresa el nombre <br />';
+    }
+
+    if(!empty($correo)){
+        $correo = filter_var($correo, FILTER_SANITIZE_EMAIL);
+
+        if(!filter_var($correo, FILTER_VALIDATE_EMAIL)){
+            $errores .= 'Por favor ingresa un correo valido <br />';
+        }
+    } else {
+        $errores .= 'Por favor ingresa un correo <br />';
+    }   
+
+    if(!empty($mensaje)){
+        $mensaje = htmlspecialchars($mensaje);
+        $mensaje = trim($mensaje);
+        $mensaje = stripslashes($mensaje);
+        
+    } else {
+        $errores .= 'Por favor ingresa el mensaje <br />';
+    }
+
+    if(!$errores){
+        $enviar_a = 'David@fondiu.com';
+        $asunto = 'Correo enviado desde mi pagina.com';
+        $mensaje_separado = "De: $nombre \n";
+        $mensaje_separado .= "Correo: $correo \n";
+        $mensaje_separado .= "Mensaje: " . $mensaje;
+
+        // mail($enviar_a, $asunto, $mensaje_preparado);
+
+        $enviado = 'true';
+    }
+}
+
+require 'index.view.php';
+
+?>
